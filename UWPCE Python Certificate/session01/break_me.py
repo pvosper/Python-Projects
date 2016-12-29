350798df58c6f6b1945f4811cece0b2a5764bc10
pvosper <pvosper@mac.com>
2016-12-29 14:31:12 -0800

    Fixing directory typo

--- UWPCE Python Certificate/session01/break_me.py
+++
@@ -1,5 +0,0 @@
-# write four simple Python functions:
-
-# Each function, when called, should cause an exception to happen
-# Each function should result in one of the four common exceptions from our
-# lecture: NameError, TypeError, SyntaxError, AttributeError
